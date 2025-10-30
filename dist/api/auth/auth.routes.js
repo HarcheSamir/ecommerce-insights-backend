692f0b3d89462afec7b8442834086b3c241ac945
HarcheSamir <harchesamir007@gmail.com>
2025-10-30 23:45:14 +0100

    Remove dist folder from repo and add to .gitignore

--- dist/api/auth/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("./auth.controller");
-const router = (0, express_1.Router)();
-router.post('/signup', auth_controller_1.authController.signUp);
-router.post('/login', auth_controller_1.authController.login);
-exports.default = router;
